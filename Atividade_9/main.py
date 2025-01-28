247c93ffb712cecfdfa6dbf9db04abc9ee4a9537
Nonolism <ruanvitormoreno@outlook.com>
2025-01-27 22:43:45 -0300

    Atv9 Utilidade de continue em laços

---
+++ Atividade_9/main.py
@@ -0,0 +1,15 @@
+def main():
+    livros = [
+    {"nome": "1984", "estoque": 5},
+    {"nome": "Dom Casmurro", "estoque": 0},
+    {"nome": "O Pequeno Príncipe", "estoque": 3},
+    {"nome": "O Hobbit", "estoque": 0},
+    {"nome": "Orgulho e Preconceito", "estoque": 2}
+]
+    
+    for livro in livros:
+        if livro['estoque'] != 0:
+            print(f'Livro disponível: {livro['nome']}')
+
+if __name__ == "__main__":
+    main()
