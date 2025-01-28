040863444a2985b878cee08e82d03e7fe98aad2e
Nonolism <ruanvitormoreno@outlook.com>
2025-01-27 21:47:00 -0300

    Atv1 Compreendendo laços

---
+++ Atividade_1/main.py
@@ -0,0 +1,8 @@
+def main():
+    clientes = ['João', 'Maria', 'Carlos', 'Ana', 'Beatriz']
+    for cliente in clientes:
+        print(cliente)
+        #Acredito que o laço for seja o mais adequado, já que nós já sabemos exatamente quantos clientes existem, ou seja, quantas vezes precisamos iterar.
+    
+if __name__ == '__main__':
+    main()
