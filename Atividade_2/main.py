18e6a52eba0f6027c31438ab87916c371fd49a5b
Nonolism <ruanvitormoreno@outlook.com>
2025-01-27 21:48:58 -0300

    Atv2 O que é um loop infinito

---
+++ Atividade_2/main.py
@@ -0,0 +1,9 @@
+def main():
+    contador = 0
+
+    while contador < 10:
+        print("Processando dados...")
+    #O erro ocorre pois não existe nada para aumentar o valor da variavel contador, logo, esse loop irá ocorrer de forma infinita.
+
+if __name__ == '__main__':
+    main()
